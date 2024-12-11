33d4b2798425423762d1dd9d8ab2777b73884c55
Deyvid Kasteh <kasteh21@gmail.com>
2024-12-11 01:13:15 -0300

    melhorias no portfolio

--- src/app/components/container3.tsx
+++ src/app/components/container3.tsx
@@ -1,136 +1,162 @@
-import React from 'react';
+import React from "react";
 
-type Container3Props = {
+type Container3Props = {};
 
-};
-
-const Container3:React.FC<Container3Props> = () => {
-
-    return (
-      <div
-        style={{
-          display: "grid",
-          gridTemplateAreas: `
+const Container3: React.FC<Container3Props> = () => {
+  return (
+    <div
+      style={{
+        display: "grid",
+        gridTemplateAreas: `
           "apresentacao portifoio"
           `,
-          gridTemplateColumns: "400px 700px",
-          gap: "10px",
+        gridTemplateColumns: "400px 700px",
+        gap: "40px",
+        height: " 640px",
+      }}
+    >
+      <div
+        style={{
+          gridArea: "apresentacao",
+          backgroundColor: "#8184d3",
+          padding: "20px",
+        }}
+      >
+        Header
+      </div>
+      <div
+        style={{
+          gridArea: "portifoio",
+          backgroundColor: "#f6f6f6",
+          // padding: "20px",
+          display: "grid",
+          gridTemplateAreas: `"header header header"
+                               "area1 area1 area2"
+                             "area1 area1 area2"
+                             "area3 area4 area4"`,
+          gridTemplateColumns: "1fr 280px 1fr",
+          gap: "40px",
         }}
       >
         <div
           style={{
-            gridArea: "apresentacao",
-            backgroundColor: "lightblue",
-            padding: "20px",
+            gridArea: "header",
+            backgroundColor: "#f6f6f6",
+            padding: "10px",
+            border: "1px solid black",
+            borderRadius: "5px",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
           }}
         >
-          Header
+          Portfolio
         </div>
         <div
           style={{
-            gridArea: "portifoio",
-            backgroundColor: "lightgreen",
-            padding: "20px",
-            display: "grid",
-            gridTemplateAreas: `"area5 area2 area3 area4"
-                             "area5 area6 area7 area8"
-                             "area5 area9 area9 area9"`,
-            gridTemplateColumns: "1fr 1fr 1fr 1fr",
-            gap: "10px",
+            gridArea: "area1",
+            background:
+              "conic-gradient(from 0deg, #ef7b78, #99d5e0, #7a876a,  #7a876a, #ef7b78)",
+            padding: "10px",
+            borderRadius: "16px",
           }}
         >
-          {/* <div
-            style={{
-              gridArea: "area1",
-              backgroundColor: "lightcoral",
-              padding: "10px",
-            }}
-          >
-            Area 1
-          </div> */}
+          Area 1
+        </div>
+        <div
+          style={{
+            gridArea: "area2",
+            // backgroundColor: "lightblue",
+            // padding: "10px",
+            display: "flex",
+            flex: "1",
+            flexDirection: "column",
+            overflow: "hidden",
+            borderRadius: "16px",
+          }}
+        >
           <div
             style={{
-              gridArea: "area2",
-              backgroundColor: "lightblue",
+              display: "flex",
+              flex: 1,
+              backgroundColor: "#b0e4de",
               padding: "10px",
+              justifyContent: "center",
+              alignItems: "center",
             }}
           >
-            Area 2
+            Area 2.1
           </div>
           <div
             style={{
-              gridArea: "area3",
-              backgroundColor: "lightgreen",
+              display: "flex",
+              flex: 1,
+              backgroundColor: "#8d6fbc",
               padding: "10px",
+              justifyContent: "center",
+              alignItems: "center",
             }}
           >
-            Area 3
-          </div>
-          <div
-            style={{
-              gridArea: "area4",
-              backgroundColor: "lightyellow",
-              padding: "10px",
-            }}
-          >
-            Area 4
-          </div>
-          <div
-            style={{
-              gridArea: "area5",
-              backgroundColor: "lightcoral",
-              padding: "10px",
-            }}
-          >
-            Area 5
-          </div>
-          <div
-            style={{
-              gridArea: "area6",
-              backgroundColor: "lightblue",
-              padding: "10px",
-            }}
-          >
-            Area 6
-          </div>
-          <div
-            style={{
-              gridArea: "area7",
-              backgroundColor: "lightgreen",
-              padding: "10px",
-            }}
-          >
-            Area 7
-          </div>
-          <div
-            style={{
-              gridArea: "area8",
-              backgroundColor: "lightyellow",
-              padding: "10px",
-            }}
-          >
-            Area 8
-          </div>
-          <div
-            style={{
-              gridArea: "area9",
-              backgroundColor: "lightcoral",
-              padding: "10px",
-            }}
-          >
-            Area 9
+            Area 2.2
           </div>
         </div>
-        {/* <div
+        <div
           style={{
-            gridArea: "footer",
-            backgroundColor: "lightgray",
-            padding: "20px",
+            gridArea: "area3",
+            backgroundColor: "#595959",
+            padding: "10px",
+            overflow: "hidden",
+            borderRadius: "16px",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
           }}
         >
-          Footer
-        </div> */}
+          Area 3
+        </div>
+        <div
+          style={{
+            gridArea: "area4",
+            // backgroundColor: "#000000",
+            // padding: "10px",
+            display: "flex",
+            flexDirection: "row",
+            overflow: "hidden",
+            borderRadius: "16px",
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              flex: 1,
+              backgroundColor: "#000000",
+              padding: "10px",
+              justifyContent: "center",
+              alignItems: "center",
+              // borderTopLeftRadius: "16px",
+              // borderBottomLeftRadius: "16px",
+            }}
+          >
+            Area 2.1
+          </div>
+          <div
+            style={{
+              display: "flex",
+              flex: 1,
+              backgroundColor: "#ffc267",
+              padding: "10px",
+              justifyContent: "center",
+              alignItems: "center",
+              
+              // borderTopRightRadius: "16px",
+              // borderBottomRightRadius: "16px",
+            }}
+          >
+            Area 4.2
+          </div>
+        </div>
       </div>
-    );
-}
+    </div>
+  );
+};
 export default Container3;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,10 +10,27 @@ export default function Home() {
     //   <h1>kasteh portfolio</h1>
     //   <Navbar />
     // </div>
-    <div style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
-      {/* <Container /> */}
-      {/* <Container2 /> */}
-      <Container3/>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#e2e2e2",
+        height: "100vh",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          backgroundColor: "#f6f6f4",
+          padding: "40px",
+          borderRadius: "20px",
+        }}
+      >
+        {/* <Container /> */}
+        {/* <Container2 /> */}
+        <Container3 />
+      </div>
     </div>
   );
 }
